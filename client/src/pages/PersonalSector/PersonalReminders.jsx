7d846369de6e6b502586a7b245ba33b2e3ecb881
Iyyanarsuriya <suriyaiyyanar10@gmail.com>
2026-02-13 12:43:30 +0530

    Personal

--- client/src/pages/PersonalSector/PersonalReminders.jsx
+++ client/src/pages/PersonalSector/PersonalReminders.jsx
@@ -51,10 +51,10 @@ const PersonalReminders = () => {
 
     const lastFetchRef = useRef(0);
 
-    const fetchData = async () => {
+    const fetchData = async (force = false) => {
         // Throttle fetching: don't fetch if last fetch was less than 60s ago
         const now = Date.now();
-        if (now - lastFetchRef.current < 60000 && !loading) {
+        if (!force && now - lastFetchRef.current < 60000 && !loading) {
             return;
         }
 
@@ -66,7 +66,7 @@ const PersonalReminders = () => {
             ]);
             setReminders(remindersRes.data);
             setUser(userRes.data);
-            setCategories(categoriesRes.data);
+            setCategories(categoriesRes.data.data || []);
             localStorage.setItem('user', JSON.stringify(userRes.data));
             lastFetchRef.current = Date.now();
         } catch (error) {
@@ -1056,10 +1056,7 @@ const PersonalReminders = () => {
                     showCategoryManager && (
                         <CategoryManager
                             categories={categories}
-                            onUpdate={() => {
-                                getCategories().then(res => setCategories(res.data));
-                                // No need to refresh reminders, but good practice
-                            }}
+                            onUpdate={() => fetchData(true)}
                             onCreate={createCategory}
                             onDelete={deleteCategory}
                             onClose={() => setShowCategoryManager(false)}
