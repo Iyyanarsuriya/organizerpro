d36e2f964f39a297228a03c8ac7e19ba99a50322
Iyyanarsuriya <suriyaiyyanar10@gmail.com>
2026-03-09 15:31:42 +0530

    update OrganizerPro UI

--- client/src/api/Attendance/attendanceApi.js
+++ client/src/api/Attendance/attendanceApi.js
@@ -1,5 +1,7 @@
 import axiosInstance from '../axiosInstance';
 
+//
+
 export const getProjects = (params) => axiosInstance.get('/projects', { params });
 export const createProject = (data) => axiosInstance.post('/projects', data);
 export const deleteProject = (id, params) => axiosInstance.delete(`/projects/${id}`, { params });
